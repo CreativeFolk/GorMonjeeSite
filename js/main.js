19a0bace553f93c0e20e7c8bf0a469a5d6a0145c
akrigline <andrew@akrigline.com>
2015-04-07 23:01:31 -0400

    Skrollr Script Fix?

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ $('#intro a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
+
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	skrollr.refresh('.tab-content');
+})
